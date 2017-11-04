e2202c8d81f721923c7d10338779e50bf2c9c2ac
Hebert Porto <hebertporto@gmail.com>
2017-11-04 11:45:03 -0200

    Feat: Criado modulo Novels

--- src/v1/config/messages.js
+++ src/v1/config/messages.js
@@ -1,6 +1,10 @@
 export const VALIDATION_ERROR = 'É necessário preencher todos os campos exigidos pela requisição'
 export const INTERNAL_ERROR = 'Um erro ocorreu durante a requisição'
 
-export const CREATE_CATEGORY_FAILED = 'Falha ao cadastrar nova categoria'
-export const UPDATE_CATEGORY_FAILED = 'Falha ao Atualizar categoria'
-export const REMOVE_CATEGORY_FAILED = 'Falha ao Remover categoria'
+export const CREATE_CHAPTER_FAILED = 'Falha ao cadastrar capítulo'
+export const UPDATE_CHAPTER_FAILED = 'Falha ao Atualizar capítulo'
+export const REMOVE_CHAPTER_FAILED = 'Falha ao Remover capítulo'
+
+export const CREATE_NOVEL_FAILED = 'Falha ao cadastrar a Novel'
+export const UPDATE_NOVEL_FAILED = 'Falha ao Atualizar a Novel'
+export const REMOVE_NOVEL_FAILED = 'Falha ao Remover a Novel'
---
+++ src/v1/models/novel/index.js
@@ -0,0 +1,134 @@
+import mongoose from './../../config/db'
+
+import {
+  CREATE_NOVEL_FAILED,
+  REMOVE_NOVEL_FAILED,
+  UPDATE_NOVEL_FAILED,
+} from './../../config/messages'
+
+const NovelSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    require: true,
+    unique: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  author: {
+    type: String,
+    required: true,
+  },
+  translation_team: {
+    type: String,
+    required: true,
+  },
+  cover_url: {
+    type: String,
+    require: true,
+  },
+  date_start: {
+    type: Date,
+  },
+  date_end: {
+    type: Date,
+  },
+  users: {
+    type: [mongoose.SchemaTypes.ObjectId],
+    ref: 'User',
+  },
+  created_at: {
+    type: Date,
+    default: Date.now,
+  },
+})
+
+export function novelModel() {
+  return mongoose.model('Novem', NovelSchema)
+}
+
+const Novel = novelModel()
+
+export function create(data) {
+  return new Novel(data)
+    .save()
+    .then(result => result)
+    .catch((err) => {
+      throw Object({ message: CREATE_NOVEL_FAILED, status: 422, payload: err })
+    })
+}
+
+export function findByIdOrFindAll(novelId, currentPage) {
+  if (novelId) {
+    const { ObjectId } = mongoose.Types
+    return Novel.findById(ObjectId(novelId))
+      .then(result => result)
+      .catch((err) => {
+        throw new Error({
+          payload: err,
+          code: 500,
+        })
+      })
+  }
+  const perPage = 2
+  const page = currentPage || 1
+
+  return Novel.find({})
+    .skip((perPage * page) - perPage)
+    .limit(perPage)
+    .then(result => (
+      Novel
+        .count()
+        .then(contResult => ({
+          novels: result,
+          currentPage: page,
+          pages: Math.ceil(contResult / perPage),
+        }))
+        .catch((err) => {
+          throw new Error({
+            payload: err,
+            code: 500,
+          })
+        })
+    ))
+    .catch((err) => {
+      throw new Error({
+        payload: err,
+        code: 500,
+      })
+    })
+}
+
+export function remove(novelId) {
+  const { ObjectId } = mongoose.Types
+
+  return Novel.remove({ _id: ObjectId(novelId) })
+    .exec()
+    .then(payload => payload)
+    .catch((err) => {
+      throw Object({
+        message: REMOVE_NOVEL_FAILED,
+        status: 422,
+        payload: err,
+      })
+    })
+}
+
+export function update(body, novelId) {
+  const { ObjectId } = mongoose.Types
+
+  return Novel.findOneAndUpdate(
+    { _id: ObjectId(novelId) },
+    { $set: body },
+    { new: true },
+  )
+    .then(payload => payload)
+    .catch((err) => {
+      throw new Error({
+        message: UPDATE_NOVEL_FAILED,
+        status: 422,
+        payload: err,
+      })
+    })
+}
