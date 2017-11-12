9c7b8ecc8c890b262549768f177e3ba1cc8aca41
Hebert Porto <hebertporto@gmail.com>
2017-11-12 16:37:47 -0200

    Add Crypto

---
+++ src/v1/config/crypto.js
@@ -0,0 +1,9 @@
+import bcrypt from 'bcryptjs'
+import Promise from 'bluebird'
+
+const saltRounds = 10
+
+export const hash = Promise
+  .promisify((password, cb) => bcrypt.hash(password, saltRounds, cb))
+
+export const compare = Promise.promisify(bcrypt.compare)
