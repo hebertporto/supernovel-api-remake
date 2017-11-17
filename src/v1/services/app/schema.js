f8bebadae496d03d32be8edca9c5bdea76a54d47
Hebert Porto <hebertporto@gmail.com>
2017-11-16 23:47:03 -0200

    Feat: Schema for requests do app

---
+++ src/v1/services/app/schema.js
@@ -0,0 +1,27 @@
+import Joi from 'joi'
+
+export const findUserByIdSchema = {
+  params: {
+    id: Joi.string(),
+  },
+}
+
+export const updateUserSchema = {
+  body: {
+    name: Joi.string(),
+    email: Joi.string().email(),
+    type: Joi.string().valid('ADIM', 'PUBLISHER', 'USER'),
+    avatar: Joi.string(),
+    facebook: Joi.string(),
+    twitter: Joi.string(),
+    gplus: Joi.string(),
+    linkedin: Joi.string(),
+    userStatus: Joi.string().valid('PENDING', 'REQUESTED', 'ACCEPTED', 'DENIED', 'BLOCKED'),
+    userId: Joi.string(),
+  },
+
+}
+
+export const registerSchema = {
+
+}
